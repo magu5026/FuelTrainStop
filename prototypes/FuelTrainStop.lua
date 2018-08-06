59719e65b3ee441b58707304ede577f78992f3e6
magu5026 <magu5026@gmail.com>
2018-08-06 12:20:27 +0200

    0.16.2 - Code rework to make it compatible with LTN.

--- prototypes/FuelTrainStop.lua
+++ prototypes/FuelTrainStop.lua
@@ -1,5 +1,6 @@
 local fts_item = table.deepcopy(data.raw['item']['train-stop'])
 fts_item.name = "fuel-train-stop"
+fts_item.icon = "__FuelTrainStop__/graphics/train-stop.png"
 fts_item.order = "a[train-system]-c[train-stop]-a[fuel-train-stop]"
 fts_item.place_result = "fuel-train-stop"
 
@@ -9,18 +10,10 @@ fts_recipe.result = "fuel-train-stop"
 
 local fts_entity = table.deepcopy(data.raw['train-stop']['train-stop'])
 fts_entity.name = "fuel-train-stop"
+fts_entity.icon = "__FuelTrainStop__/graphics/train-stop.png"
 fts_entity.minable.result = "fuel-train-stop"
 
 data:extend({fts_item,fts_recipe,fts_entity})
 
-data.raw['technology']['automated-rail-transportation'].effects =
-    {
-      {
-        type = "unlock-recipe",
-        recipe = "train-stop"
-      },
-	  {
-        type = "unlock-recipe",
-        recipe = "fuel-train-stop"
-      }	  
-    }
+
+table.insert(data.raw['technology']['automated-rail-transportation'].effects,{type = "unlock-recipe",recipe = "fuel-train-stop"})
