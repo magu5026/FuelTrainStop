e0e679f7440b460f678fa535896e7c4e3149703e
magu5026 <>
2026-06-28 17:23:16 +0200

    Version: 0.18.1
    Date: 2020-01-31
      Major Features:
        - Release 0.18.

--- data.lua
+++ data.lua
@@ -1,34 +1,18 @@
 table.insert(data.raw.technology['automated-rail-transportation'].effects,{type = "unlock-recipe",recipe = "fuel-train-stop"})
 
-data:extend({
-util.merge{data.raw['train-stop']['train-stop'],
-		{
-			name = "fuel-train-stop",
-			icon = "__FuelTrainStop__/graphics/train-stop.png",
-			minable = {mining_time = 0.2, result = "fuel-train-stop"}
-		}
-	},
-	{
-		type = "item",
-		name = "fuel-train-stop",
-		icon = "__FuelTrainStop__/graphics/train-stop.png",
-		icon_size = 32,
-		subgroup = "transport",
-		order = "a[train-system]-c[train-stop]-a[fuel-train-stop]",
-		place_result = "fuel-train-stop",
-		stack_size = 10
-	},
-	{
-		type = "recipe",
-		name = "fuel-train-stop",
-		enabled = false,
-		ingredients =
-			{
-				{"electronic-circuit", 5},
-				{"iron-plate", 6},
-				{"iron-stick", 6},
-				{"steel-plate", 3}
-			},
-		result = "fuel-train-stop"
-	}
-})
+local train_stop_entity = table.deepcopy(data.raw['train-stop']['train-stop'])
+train_stop_entity.name = "fuel-train-stop"
+train_stop_entity.minable.result = "fuel-train-stop"
+
+local train_stop_item = table.deepcopy(data.raw['item']['train-stop'])
+train_stop_item.name = "fuel-train-stop"
+train_stop_item.icon = nil
+train_stop_item.icons = { {icon = "__base__/graphics/icons/train-stop.png", tint = {r=0, g=0.7, b=1, a=1}} }
+train_stop_item.order = "a[train-system]-c[train-stop]-a[fuel-train-stop]"
+train_stop_item.place_result = "fuel-train-stop"
+
+local train_stop_recipe = table.deepcopy(data.raw['recipe']['train-stop'])
+train_stop_recipe.name = "fuel-train-stop"
+train_stop_recipe.result = "fuel-train-stop"
+
+data:extend({train_stop_entity,train_stop_item,train_stop_recipe})
